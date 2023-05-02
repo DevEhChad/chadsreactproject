60ff7f7e84e32c26d745c1b5aea4bcd9b1348eb7
DevEhChad <devehchad@gmail.com>
2023-05-02 16:32:08 -0400

    fourth commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,23 +18,3 @@ function Home() {
 }
 
 export default Home;
-import React from "react";
-import { Link } from "react-router-dom";
-import BannerImage from "../assets/Tokyo-Ghoul-Backgrounds.jpg";
-import "../styles/Home.css";
-
-function Home() {
-  return (
-    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
-      <div className="headerContainer">
-        <h1> Chad's Test Website </h1>
-        <p> This is kinda fun</p>
-        <Link to='shop'>
-          <button> This is a button </button>
-        </Link>
-      </div>
-    </div>
-  );
-}
-
-export default Home;
