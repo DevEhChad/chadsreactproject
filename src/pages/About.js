146e1ecc47a14728991f2af194305158a0e51bed
EhChad <ehchad@pop-os.localdomain>
2023-11-22 15:30:01 -0500

    From Laptop Machine Commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,11 +11,16 @@ function About() {
       <div className="aboutBottom">
         <h1> ABOUT US</h1>
         <p>
-          Hello, My name is Chad and I'm currently working towards my goals on trying to become a developer. 
-          I been working hard and trying to learn quickly. 
-          I try to be patient but I really wanna just learn fast.
+          Hello, My name is Chad and I'm currently working towards my goals on trying to become a developer.
+          If you have any pointers and are a gamer please join the discord! 
         </p>
       </div>
+      <div className="discordinvabout">
+      <a class="btn" href="https://discord.gg/brQXPhQ" target="_blank" rel="noopener noreferrer">Join The Discord!</a>
+    </div>
+      <div className="discordintabout">
+        <iframe src="https://discord.com/widget?id=573633846587817992&theme=dark" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+        </div>
     </div>
   );
 }
