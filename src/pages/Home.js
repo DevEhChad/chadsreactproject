9f52f6dfe158a58ff8eaa965798231f81ed2d6fa
Dev EhChad <devehchad@gmail.com>
2024-01-25 01:52:22 -0500

    More updates
    More general updates for practice

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BannerImage from "../assets/MytwitchBanner.png";
+import BannerImage from "../assets/BannerTemplate.jpg";
 import "../styles/Home.css";
 
 function Home() {
